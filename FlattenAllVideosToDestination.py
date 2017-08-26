d0978d6406f1e54ebad3626fbb7c42f8e58bff95
Ryan Graham <ryan@graham.pub>
2017-08-25 20:31:14 -0400

    ignore the 2 csvs that are generated, turned the python script into OO.

--- FlattenAllVideosToDestination.py
+++ FlattenAllVideosToDestination.py
@@ -2,33 +2,39 @@
 import os
 import shutil
 
-source = '//cerberus/download/'
-destination = '//cerberus/download/'
+class FlattenHelper:
+	#source = '//cerberus/download/'
+	#destination = '//cerberus/download/'
+	source = "./test/"
+	destination = "./test/"
 
-extensions = set(["mp4","avi","mkv","mov","mpg","wmv"])
-notMigratedList = "notMigrated.csv"
-migratedList = "migrated.csv"
-numberOfMoviesMigrated = 0
-numberOfFilesFound = 0
+	extensions = set(["mp4","avi","mkv","mov","mpg","wmv"])
+	notMigratedList = "notMigrated.csv"
+	migratedList = "migrated.csv"
+	numberOfMoviesMigrated = 0
+	numberOfFilesFound = 0
 
-with open(notMigratedList, 'w') as notMigratedFileList:	
-	with open(migratedList, 'w') as migratedFileList:	
-		# traverse root directory, and list directories as dirs and files as files
-		for root, dirs, files in os.walk(source):
-			path = root.split(os.sep)
-			for file in files:
-				numberOfFilesFound += 1
-				moveFrom = os.path.join(root,file)
-				moveTo = os.path.join(source,file)
-				fileExtension = file.rsplit('.',1)[1]
-				if (moveFrom != moveTo):
-					if (fileExtension in extensions):
-						print("moving " + moveFrom + " to " + moveTo)
-						migratedFileList.write(moveFrom + "," + str(moveFrom).rsplit('.',1)[1]+"\n")
-						shutil.move(moveFrom, moveTo)
-						numberOfMoviesMigrated += 1
-					else:
-						notMigratedFileList.write(moveFrom + "," + str(moveFrom).rsplit('.',1)[1]+"\n")
-						
-print("completed migrating.")
-print("migrated " + str(numberOfMoviesMigrated) + " of " + str(numberOfFilesFound) + " files")
+	def flattenTheVideos(self):
+		with open(FlattenHelper.notMigratedList, 'w') as notMigratedFileList:	
+			with open(FlattenHelper.migratedList, 'w') as migratedFileList:	
+				# traverse root directory, and list directories as dirs and files as files
+				for root, dirs, files in os.walk(FlattenHelper.source):
+					path = root.split(os.sep)
+					for file in files:
+						FlattenHelper.numberOfFilesFound += 1
+						moveFrom = os.path.join(root,file)
+						moveTo = os.path.join(FlattenHelper.destination,file)
+						fileExtension = file.rsplit('.',1)[1]
+						if (moveFrom != moveTo):
+							if (fileExtension in FlattenHelper.extensions):
+								print("moving " + moveFrom + " to " + moveTo)
+								migratedFileList.write(moveFrom + "," + str(moveFrom).rsplit('.',1)[1]+"\n")
+								#shutil.move(moveFrom, moveTo)
+								FlattenHelper.numberOfMoviesMigrated += 1
+							else:
+								notMigratedFileList.write(moveFrom + "," + str(moveFrom).rsplit('.',1)[1]+"\n")
+								
+		print("Completed migrating.\n" + "Migrated " + str(FlattenHelper.numberOfMoviesMigrated) + " of " + str(FlattenHelper.numberOfFilesFound) + " files")
+		
+flattenHelper = FlattenHelper()
+flattenHelper.flattenTheVideos()
