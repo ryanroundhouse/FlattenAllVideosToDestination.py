a45f74b96d81eb2d67abf613c1f40d5fb6d8708a
Ryan Graham <rg@ryangraham.ca>
2020-11-14 10:37:40 -0500

    refactored into files and folders.

--- FlattenUtils_test.py
+++ FlattenUtils_test.py
@@ -17,37 +17,37 @@ class TestFlattenUtils(unittest.TestCase):
             FlattenUtils.getFilesToMigrate("./test/1/herewego.mp4", [])
 
     def test_getFilesToMigrateReturnsAllFilesWhenProvidedPath(self):
-        path = ".\\test\\"
+        path = "./test/"
         pwd = os.path.abspath(os.path.curdir)
         expectedOutput = [
-            os.path.join(pwd, "test\\1\\herewego.mp4"),
-            os.path.join(pwd, "test\\1\\movie.txt"),
-            os.path.join(pwd, "test\\1\\season 1\\episodes\\s01e01.mkv"),
-            os.path.join(pwd, "test\\2\\movie.mkv")
+            os.path.join(pwd, "test/1/movie.txt"),
+            os.path.join(pwd, "test/1/herewego.mp4"),
+            os.path.join(pwd, "test/1/season 1/episodes/s01e01.mkv"),
+            os.path.join(pwd, "test/2/movie.mkv")
         ]
-        self.assertEqual(FlattenUtils.getFilesToMigrate(path, []), expectedOutput)
+        self.assertEqual(FlattenUtils.getFilesToMigrate(path, ['.mp4', '.mkv', '.txt']), expectedOutput)
 
     def test_getFilesToMigrateReturnsOnlyMP4WhenProvidedPathAndMP4Filter(self):
-        path = ".\\test\\1\\"
+        path = "./test/1/"
         pwd = os.path.abspath(os.path.curdir)
         expectedOutput = [
-            os.path.join(pwd, "test\\1\\herewego.mp4")
+            os.path.join(pwd, "test/1/herewego.mp4")
         ]
         self.assertEqual(FlattenUtils.getFilesToMigrate(path, [".mp4"]), expectedOutput)
 
     def test_getFilesToMigrateReturnsFilesFromSubfolders(self):
-        path = ".\\test\\1\\"
+        path = "./test/1/"
         pwd = os.path.abspath(os.path.curdir)
         expectedOutput = [
-            os.path.join(pwd, "test\\1\\season 1\\episodes\\s01e01.mkv")
+            os.path.join(pwd, "test/1/season 1/episodes/s01e01.mkv")
         ]
         self.assertEqual(FlattenUtils.getFilesToMigrate(path, [".mkv"]), expectedOutput)
 
     def test_getFilesToMigrateReturnsTwoFilesFromSubfolders(self):
-        path = ".\\test\\"
+        path = "./test/"
         pwd = os.path.abspath(os.path.curdir)
         expectedOutput = [
-            os.path.join(pwd, "test\\1\\season 1\\episodes\\s01e01.mkv"),
-            os.path.join(pwd, "test\\2\\movie.mkv")
+            os.path.join(pwd, "test/1/season 1/episodes/s01e01.mkv"),
+            os.path.join(pwd, "test/2/movie.mkv")
         ]
         self.assertEqual(FlattenUtils.getFilesToMigrate(path, [".mkv"]), expectedOutput)
