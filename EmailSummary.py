cf08402afbcc77e614f354e1cb3873f5a1608f21
Ryan Graham <rg@ryangraham.ca>
2020-11-14 20:51:12 -0500

    added html to emails

--- EmailSummary.py
+++ EmailSummary.py
@@ -1,6 +1,7 @@
 import smtplib
 import config
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 def sendEmailSummary(smallFiles, bigFiles):
     """ Sends an email summary of all small files and all big files. """
@@ -9,18 +10,41 @@ def sendEmailSummary(smallFiles, bigFiles):
     if not bigFiles:
         raise Exception("You must send a list of big files")
 
-    message = """ \
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "File sort summary"
+    msg['From'] = config.sender
+    msg['To'] = config.receiver
+
+    textMessage = """ \
 Files sent to Movies:
-{1}
+{0}
 
 Files sent to Series:
-{2}
-"""
-    messageWithHeader = 'Subject:File sort summary\n\n{0}'.format(message)
+{1}
+""".format('\n'.join(bigFiles), '\n'.join(smallFiles))
+
+    htmlMessage = """ \
+<html>
+    <head></head>
+    <body>
+        <h2>Files sent to Movies:</h2>
+        {0}
+
+        <h2>Files sent to Series:</h2>
+        {1}
+    </body>
+</html>
+""".format('\n'.join(bigFiles), '\n'.join(smallFiles))
+
+    part1 = MIMEText(textMessage, 'plain')
+    part2 = MIMEText(htmlMessage, 'html')
+
+    msg.attach(part1)
+    msg.attach(part2)
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(config.username, config.password)
-        server.sendmail(config.sender, config.receiver, messageWithHeader.format(config.sender, '\n'.join(bigFiles), '\n'.join(smallFiles)))
+        server.sendmail(config.sender, config.receiver, msg.as_string())
 
 #smallFiles = ["first.mkv", "second.mkv"]
 #bigFiles = ["big1.mkv", "big2.avi"]
